b200041817e19cd9c33c9a00a8c07abb70b3b714
OlgPro <delphi73@web.de>
2023-01-06 19:10:33 +0100

    DFS BFS as class

--- DFS.dpr
+++ DFS.dpr
@@ -5,115 +5,9 @@
 {$R *.res}
 
 uses
-  System.SysUtils, Generics.Collections;
-
-type
-
-  TGraph = class
-    private
-      FSize: Integer;
-      FList: TList<TList<Integer>>;
-    public
-      constructor Create(ASize: Integer);
-      destructor Destroy; override;
-
-      procedure Add(AId: Integer; AEdge: Integer);
-
-      procedure DFS(AStartEdge: Integer; AVisitedList: TList<Boolean>);
-      procedure BFS(AStartEdge: Integer; AVisitedList: TList<Boolean>);
-  end;
-
-{ TGraph }
-
-constructor TGraph.Create(ASize: Integer);
-var i: Integer;
-    L: TList<Integer>;
-begin
-  FSize := ASize;
-  FList := TList<TList<Integer>>.Create;
-
-  for i := 0 to ASize - 1 do
-    begin
-      L := TList<Integer>.Create;
-      FList.Add(L);
-    end;
-end;
-
-destructor TGraph.Destroy;
-begin
-
-  FreeAndNil(FList);
-
-  inherited;
-end;
-
-procedure TGraph.Add(AId, AEdge: Integer);
-begin
-  FList[AId].Add(AEdge);
-end;
-
-procedure TGraph.DFS(AStartEdge: Integer; AVisitedList: TList<Boolean>);
-var Stack: TStack<Integer>;
-    S, Q: Integer;
-begin
-
-  Stack := TStack<Integer>.Create;
-  Stack.Push(AStartEdge);
-
-  while Stack.Count > 0 do
-    begin
-      S := Stack.Pop;
-
-      if not AVisitedList[S] then
-        begin
-          Write(S, ' ');
-          AVisitedList[S] := True;
-        end;
-
-      for Q in FList[S] do
-        begin
-          if not AVisitedList[Q] then
-            Stack.Push(Q);
-        end;
-
-    end;
-
-  Writeln;
-
-  FreeAndNil(Stack);
-end;
-
-procedure TGraph.BFS(AStartEdge: Integer; AVisitedList: TList<Boolean>);
-var Queue: TQueue<Integer>;
-    S, Q: Integer;
-begin
-
-  Queue := TQueue<Integer>.Create;
-  Queue.Enqueue(AStartEdge);
-
-  AVisitedList[AStartEdge] := True;
-
-  while Queue.Count > 0 do
-    begin
-      S := Queue.Dequeue;
-      Write(S, ' ');
-
-      for Q in FList[S] do
-        begin
-          if not AVisitedList[Q] then
-            begin
-              AVisitedList[Q] := True;
-              Queue.Enqueue(Q);
-            end;
-        end;
-
-    end;
-
-  Writeln;
-
-  FreeAndNil(Queue);
-
-end;
+  System.SysUtils,
+  Generics.Collections,
+  TreeGraph in 'TreeGraph.pas';
 
 procedure PrintVisiteList(AVisitedList: TList<Boolean>);
 var B: Boolean;
@@ -123,11 +17,10 @@ begin
     Write(Integer(B), ' ');
 
   Writeln;
-
 end;
 
-var G: TGraph;
-    VisitedList: TList<Boolean>;
+var G: TTreeGraph;
+    DataList: TList<Integer>;
     N: Integer;
     i: Integer;
     B: Boolean;
@@ -135,16 +28,9 @@ begin
 
   N := 12;
 
-  VisitedList := TList<Boolean>.Create;
+  DataList := TList<Integer>.Create;
 
-  for i := 1 to N do
-    begin
-      VisitedList.Add(False);
-    end;
-
-  PrintVisiteList(VisitedList);
-
-  G := TGraph.Create(N);
+  G := TTreeGraph.Create(N);
 
   G.Add(0, 1);
   G.Add(0, 3);
@@ -160,34 +46,31 @@ begin
   G.Add(8, 10);
 
   Writeln('-- DFS --');
-  for i := 0 to VisitedList.Count - 1 do
+  for i := 0 to G.VisitedList.Count - 1 do
     begin
-      if not VisitedList[i] then
+      if not G.VisitedList[i] then
         begin
-          G.DFS(i, VisitedList);
-          PrintVisiteList(VisitedList);
+          G.DFS(i, DataList);
+          PrintVisiteList(G.VisitedList);
         end;
     end;
 
-  for i := 0 to N - 1 do
-    begin
-      VisitedList[i] := False;
-    end;
+  G.Reset;
 
   Writeln;
   Writeln('-- BFS --');
 
-  for i := 0 to VisitedList.Count - 1 do
+  for i := 0 to G.VisitedList.Count - 1 do
     begin
-      if not VisitedList[i] then
+      if not G.VisitedList[i] then
         begin
-          G.BFS(i, VisitedList);
-          PrintVisiteList(VisitedList);
+          G.BFS(i, DataList);
+          PrintVisiteList(G.VisitedList);
         end;
     end;
 
   FreeAndNil(G);
-  FreeAndNil(VisitedList);
+  FreeAndNil(DataList);
 
   Readln;
 
---
+++ TreeGraph.pas
@@ -0,0 +1,138 @@
+﻿unit TreeGraph;
+
+interface
+uses
+
+  System.SysUtils,
+  Generics.Collections;
+
+type
+
+  TTreeGraph = class
+    private
+      FSize: Integer;
+      FList: TList<TList<Integer>>;
+
+      FVisitedList: TList<Boolean>;
+    public
+      constructor Create(ASize: Integer);
+      destructor Destroy; override;
+
+      procedure Add(AId: Integer; AEdge: Integer);
+
+      procedure DFS(AStartEdge: Integer; AIndexes: TList<Integer>);
+      procedure BFS(AStartEdge: Integer; AIndexes: TList<Integer>);
+
+      procedure Reset;
+
+      property VisitedList: TList<Boolean> read FVisitedList;
+  end;
+
+implementation
+
+{ TTreeGraph }
+
+constructor TTreeGraph.Create(ASize: Integer);
+var i: Integer;
+    L: TList<Integer>;
+begin
+  FSize := ASize;
+  FList := TList<TList<Integer>>.Create;
+  FVisitedList := TList<Boolean>.Create;
+
+  for i := 0 to ASize - 1 do
+    begin
+      L := TList<Integer>.Create;
+      FList.Add(L);
+
+      FVisitedList.Add(False);
+    end;
+end;
+
+destructor TTreeGraph.Destroy;
+begin
+
+  FreeAndNil(FList);
+
+  inherited;
+end;
+
+procedure TTreeGraph.Add(AId, AEdge: Integer);
+begin
+  FList[AId].Add(AEdge);
+end;
+
+procedure TTreeGraph.DFS(AStartEdge: Integer; AIndexes: TList<Integer>);
+var Stack: TStack<Integer>;
+    S, Q: Integer;
+begin
+
+  Stack := TStack<Integer>.Create;
+  Stack.Push(AStartEdge);
+
+  while Stack.Count > 0 do
+    begin
+      S := Stack.Pop;
+
+      if not FVisitedList[S] then
+        begin
+          Write(S, ' ');
+          AIndexes.Add(S);
+          FVisitedList[S] := True;
+        end;
+
+      for Q in FList[S] do
+        begin
+          if not FVisitedList[Q] then
+            Stack.Push(Q);
+        end;
+
+    end;
+
+  Writeln;
+
+  FreeAndNil(Stack);
+end;
+
+
+procedure TTreeGraph.BFS(AStartEdge: Integer; AIndexes: TList<Integer>);
+var Queue: TQueue<Integer>;
+    S, Q: Integer;
+begin
+
+  Queue := TQueue<Integer>.Create;
+  Queue.Enqueue(AStartEdge);
+
+  FVisitedList[AStartEdge] := True;
+
+  while Queue.Count > 0 do
+    begin
+      S := Queue.Dequeue;
+      Write(S, ' ');
+      AIndexes.Add(S);
+
+      for Q in FList[S] do
+        begin
+          if not FVisitedList[Q] then
+            begin
+              FVisitedList[Q] := True;
+              Queue.Enqueue(Q);
+            end;
+        end;
+
+    end;
+
+  Writeln;
+
+  FreeAndNil(Queue);
+
+end;
+
+procedure TTreeGraph.Reset;
+var i: Integer;
+begin
+  for i := 0 to FVisitedList.Count - 1 do
+    FVisitedList[i] := False;
+end;
+
+end.
